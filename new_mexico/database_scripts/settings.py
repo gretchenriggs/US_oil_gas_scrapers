bde4941812949bed27840e541fd6802d39e5ecfa
Greg <gdl5014@gmail.com>
2017-09-08 17:20:51 -0600

    rest of files

---
+++ new_mexico/database_scripts/settings.py
@@ -0,0 +1,9 @@
+DATABASE = {
+        'drivername': 'postgres',
+        'host': 'localhost',
+        'port': '5433',
+        'username': 'greg',
+        'password': 'lackey',
+        'database': 'new_mexico',
+}
+
