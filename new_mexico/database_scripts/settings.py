0837c5a992ec46f2db4f29c906d5ceae0cd332fa
Geo Dancer <geodancer@gmail.com>
2017-09-30 16:04:51 -0600

    Made states data folder. Added Utah data.

--- new_mexico/database_scripts/settings.py
+++
@@ -1,9 +0,0 @@
-DATABASE = {
-        'drivername': 'postgres',
-        'host': 'localhost',
-        'port': '5433',
-        'username': 'greg',
-        'password': 'lackey',
-        'database': 'new_mexico',
-}
-
